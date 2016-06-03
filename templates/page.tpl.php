49f611f9954c57708350a3d2a62429304e40bcf8
Joseph Zhao <joseph.zhao@xing.net.au>
2016-06-03 15:24:54 +1000

    Update main area.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -7,45 +7,47 @@
 <header id="header" class="header <?php print $container_class; ?>" role="banner">
   <?php print render($page['header']); ?>
 </header><!-- /#page-header -->
-<div id="main" class="main-container <?php print $container_class; ?>">
-  <?php if (!empty($page['highlighted'])): ?>
-    <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-  <?php endif; ?>
-  <?php print $messages; ?>
-  <div id="content" class="row">
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
+<main>
+  <div id="main" class="main-container <?php print $container_class; ?>">
+    <?php if (!empty($page['highlighted'])): ?>
+      <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
     <?php endif; ?>
-    <section id="main-content-section" class="<?php print $content_column_class; ?>" role="main">
-      <a id="main-content"></a>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
+    <?php print $messages; ?>
+    <div id="content" class="row">
+      <?php if (!empty($page['sidebar_first'])): ?>
+        <aside class="col-sm-3" role="complementary">
+          <?php print render($page['sidebar_first']); ?>
+        </aside>  <!-- /#sidebar-first -->
       <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
+      <section id="main-content-section" class="<?php print $content_column_class; ?>" role="main">
+        <a id="main-content"></a>
+        <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+        <?php print render($title_prefix); ?>
+        <?php if (!empty($title)): ?>
+          <h1 class="page-header"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php if (!empty($tabs)): ?>
+          <?php print render($tabs); ?>
+        <?php endif; ?>
+        <?php if (!empty($page['help'])): ?>
+          <?php print render($page['help']); ?>
+        <?php endif; ?>
+        <?php if (!empty($action_links)): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <div id="page-content">
+          <?php print render($page['content']); ?>
+        </div>
+      </section>
+      <?php if (!empty($page['sidebar_second'])): ?>
+        <aside class="col-sm-3" role="complementary">
+          <?php print render($page['sidebar_second']); ?>
+        </aside>  <!-- /#sidebar-second -->
       <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <div id="page-content">
-        <?php print render($page['content']); ?>
-      </div>
-    </section>
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
-    <?php endif; ?>
+    </div>
   </div>
-</div>
+</main>
 <?php if (!empty($page['footer'])): ?>
   <footer id="footer" class="footer <?php print $container_class; ?>">
     <?php print render($page['footer']); ?>
